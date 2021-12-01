918574ff1e30960dc98449222c985823c164e547
david.jones <david.jones@datadoghq.com>
2021-12-01 10:59:09 +0000

    initial commit

--- src/scripts/main-dd-js.js
+++ src/scripts/main-dd-js.js
@@ -5,13 +5,15 @@ import './datadog-docs';
 
 import './components/copy-code';
 import './components/global-modals';
-import './components/header';
+// import './components/header';
 import './components/platforms';
 import './components/algolia';
 import './components/api';
 import './components/code-languages';
 import './components/language-select';
 import './components/bootstrap-dropdown-custom';
+import './components/navbar'; // should move this to websites-modules
+import './components/mobile-nav'; // should move this to websites-modules
 
 // TODO: split up code from datadog-docs.js into modules after webpack migration
 // import './components/sidenav';
